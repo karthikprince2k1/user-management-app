1e71a0886d4cc2c302171bfa880f086ebd2b0fd3
Ganesh Karthik Natarajan <karthikprince2k1@gmail.com>
2020-07-17 22:15:42 -0400

    Created the initial App

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./App.css";
+
+import Nav from "./components/Nav";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>User Management App</h1>
+      <Nav />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Nav.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import Home from "../pages/Home";
+import CreateUpdateUser from "../pages/CreateUpdateUser";
+import CreateUpdateContact from "../pages/CreateUpdateContact";
+import ViewUsers from "../pages/ViewUsers";
+import NotFound from "../pages/NotFound";
+import "../styles/menu.css";
+
+export default function (props) {
+  return (
+    <Router>
+      <div>
+        <ul className="menu">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/createuser">Create User</Link>
+          </li>
+          <li>
+            <Link to="/createcontact">Create Contact</Link>
+          </li>
+          <li>
+            <Link to="/viewusers">View Users</Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/createuser" component={CreateUpdateUser} />
+          <Route path="/createcontact" component={CreateUpdateContact} />
+          <Route path="/viewusers" component={ViewUsers} />
+          <Route default component={NotFound} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
