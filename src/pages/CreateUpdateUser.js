b75656bede494f487c753c00b5c1e9a9f5b8b3d7
Ganesh Karthik Natarajan <karthikprince2k1@gmail.com>
2020-07-18 17:43:01 -0400

    added the common folder

--- src/pages/CreateUpdateUser.js
+++ src/pages/CreateUpdateUser.js
@@ -1,5 +1,20 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import Input from "../common/src/components/Input/Input";
+import DropDown from "../common/src/components/DropDown/DropDown";
 
-export default function (props) {
-  return <h1>Create/Update User</h1>;
+export default function App() {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => console.log(data);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <DropDown label="Suffix" ref={register} options={["Mr.", "Mrs."]} />
+      <Input label="First Name" register={register} required />
+      <Input label="Last Name" register={register} required />
+      <Input label="Email" register={register} required />
+
+      <input type="submit" />
+    </form>
+  );
 }
