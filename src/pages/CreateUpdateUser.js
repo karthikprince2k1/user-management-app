cafa616061221ce1b2114b6b0293dd6d48b92fb1
Ganesh Karthik Natarajan <karthikprince2k1@gmail.com>
2020-07-18 23:46:27 -0400

    updated the CreateUpdateUser page with Datepicker

--- src/pages/CreateUpdateUser.js
+++ src/pages/CreateUpdateUser.js
@@ -2,17 +2,28 @@ import React from "react";
 import { useForm } from "react-hook-form";
 import Input from "../common/src/components/Input/Input";
 import DropDown from "../common/src/components/DropDown/DropDown";
+import CustomDatePicker from "../common/src/components/CustomDatePicker/CustomDatePicker";
 
 import { yupResolver } from "@hookform/resolvers";
 import * as yup from "yup";
 
 import "../styles/createUser.css";
+const phoneRegExp = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
 
 const schema = yup.object().shape({
   suffix: yup.string().required(),
   firstName: yup.string().required(),
   lastName: yup.string().required(),
   email: yup.string().email().required(),
+  dateOfBirth: yup.string().required(),
+  // gender: yup.string().required,
+  // role: yup.string().required,
+  // contacts: yup.array().of(
+  //   yup.object().shape({
+  //     type: yup.string().required,
+  //     contact: yup.string().matches(phoneRegExp, "Phone number is not valid"),
+  //   })
+  // ),
 });
 
 export default function App() {
@@ -20,7 +31,6 @@ export default function App() {
     resolver: yupResolver(schema),
   });
   const onSubmit = (data) => console.log(data);
-
   return (
     <form onSubmit={handleSubmit(onSubmit)} className="create-user-form">
       <DropDown
@@ -41,6 +51,13 @@ export default function App() {
       <p>{errors.lastName?.message}</p>
       <Input label="Email*" register={register} name="email" required />
       <p>{errors.email?.message}</p>
+      <CustomDatePicker
+        label="Date of Birth*"
+        name="dateOfBirth"
+        register={register}
+        required
+      />
+      <p>{errors.dateOfBirth?.message}</p>
       <input type="submit" />
     </form>
   );
