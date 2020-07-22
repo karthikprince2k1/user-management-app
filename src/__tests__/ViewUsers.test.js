274bc2099da54303c862940cae214ddad21362a9
Ganesh Karthik Natarajan <karthikprince2k1@gmail.com>
2020-07-22 01:16:43 -0400

    Added the basic test for ViewUsers Screen

---
+++ src/__tests__/ViewUsers.test.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Router } from "react-router-dom";
+import { createStore } from "redux";
+import { createMemoryHistory } from "history";
+import { render, fireEvent, cleanup, waitForDomChange } from "../test-utils";
+import "@testing-library/jest-dom/extend-expect";
+import App from "../App";
+
+afterEach(cleanup);
+
+describe("View Users Route test Suite", () => {
+  it("should render the view users screen with data from store", async () => {
+    const store = createStore(() => {
+      users: [
+        {
+          user_id: 5,
+          suffix: "Mr.",
+          firstname: "ricky",
+          lastname: "karthik",
+          email: "ricky.karthik@gmail.com",
+          dateofbirth: "2020-07-04",
+          gender: "male",
+          role: "read;admin",
+        },
+      ];
+    });
+    const history = createMemoryHistory();
+    history.push("/viewusers");
+    const { getByText, debug } = render(
+      <Router history={history}>
+        <App />
+      </Router>
+    );
+
+    await waitForDomChange();
+
+    debug();
+
+    const ricky = getByText("ricky");
+    expect(ricky).toBeInTheDocument();
+  });
+});
